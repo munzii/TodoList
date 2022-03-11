5e75bbcc436f2f660269c8cdaebe43a74470599a
munzii <munzii422@gmail.com>
2022-03-11 21:59:15 +0900

    feature

--- src/test/java/org/example/service/TodoServiceTest.java
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -9,8 +9,17 @@ import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
@@ -31,18 +40,34 @@ class TodoServiceTest {
         TodoEntity actual = this.todoService.add(expected);
 
         assertEquals(expected.getTitle(), actual.getTitle());
-    }
 
-    private void assertEquals() {
-    }
-
-    private Object any(Class<TodoEntity> todoEntityClass) {
-    }
-
-    private void when() {
     }
 
     @Test
     void searchById() {
+        TodoEntity entity = new TodoEntity();
+        entity.setId(123L);
+        entity.setTitle("TITLE");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong())).willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+
+        TodoEntity expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getOrder(), actual.getOrder());
+        assertEquals(expected.getCompleted(), actual.getCompleted());
+
+    }
+
+    @Test
+    public void searchByIdFailed() {
+        given(this.todoRepository.findById(anyLong())).willReturn(Optional.empty());
+        assertThrows(ResponseStatusException.class, () -> { this.todoService.searchById(123L);});
     }
 }
